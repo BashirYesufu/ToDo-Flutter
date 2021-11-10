5bcf62f84418c8f0c5be47a11c9fa92d905237a8
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-10 11:34:36 +0100

    Added the three main buttons to the screen's bottom

--- kobo_todo_app/lib/home.dart
+++ kobo_todo_app/lib/home.dart
@@ -12,35 +12,39 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
 
-            GradientButton(
-              color1: Color(0xff6C37CB),
-              color2: Color(0xffE716A3),
-              icon: Icons.check,
-            ),
+              GradientButton(
+                color1: Color(0xff6C37CB),
+                color2: Color(0xffE716A3),
+                icon: Icons.check,
+              ),
 
-            ElevatedButton(
-              onPressed: () {},
-              child: Icon(Icons.calendar_today_outlined, color: Colors.orange[800], size: 30,),
-              style: ElevatedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(20.0),
-                primary: Colors.white
-              )
-            ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Icon(Icons.calendar_today_outlined, color: Colors.orange[800], size: 30,),
+                style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(20.0),
+                  primary: Colors.white
+                )
+              ),
 
-            GradientButton(
-              color1: Color(0xff0D7AEA),
-              color2: Color(0xff00D5F9),
-              icon: Icons.add,
-            ),
+              GradientButton(
+                color1: Color(0xff0D7AEA),
+                color2: Color(0xff00D5F9),
+                icon: Icons.add,
+              ),
 
-          ],
-        ),
+            ],
+          ),
+          SizedBox(height: 40.0,)
+        ],
       ),
     );
   }
