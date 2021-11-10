281ddcb68d48bb0de038c8bca309fb144f402416
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-11 00:58:02 +0100

    created card for todos

--- kobo_todo_app/lib/home.dart
+++ kobo_todo_app/lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'gradient_button.dart';
+import 'todo_card.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,40 +13,57 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
 
-              GradientButton(
-                color1: Color(0xff6C37CB),
-                color2: Color(0xffE716A3),
-                icon: Icons.check,
-              ),
+      backgroundColor: Colors.grey,
+      persistentFooterButtons: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
 
-              ElevatedButton(
+            GradientButton(
+              color1: Color(0xff6C37CB),
+              color2: Color(0xffE716A3),
+              action: () {},
+              icon: Icons.check,
+            ),
+
+            ElevatedButton(
                 onPressed: () {},
                 child: Icon(Icons.calendar_today_outlined, color: Colors.orange[800], size: 30,),
                 style: ElevatedButton.styleFrom(
-                  shape: CircleBorder(),
-                  padding: EdgeInsets.all(20.0),
-                  primary: Colors.white
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(20.0),
+                    primary: Colors.white
                 )
-              ),
+            ),
 
-              GradientButton(
-                color1: Color(0xff0D7AEA),
-                color2: Color(0xff00D5F9),
-                icon: Icons.add,
-              ),
+            GradientButton(
+              color1: Color(0xff0D7AEA),
+              color2: Color(0xff00D5F9),
+              action: () {},
+              icon: Icons.add,
+            ),
 
-            ],
-          ),
-          SizedBox(height: 40.0,)
-        ],
+          ],
+        ),
+      ],
+
+
+
+      body: SingleChildScrollView(
+        child: Column(
+
+              children: [
+                SizedBox(height: 100.0,),
+               TodoCard(),
+                TodoCard(),
+                TodoCard(),
+                TodoCard(),
+
+              ],
+            ),
       ),
-    );
+      );
   }
 }
---
+++ kobo_todo_app/lib/todo_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'gradient_button.dart';
+
+class TodoCard extends StatelessWidget {
+  const TodoCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      height: 70.0,
+      padding: EdgeInsets.fromLTRB(0.0, 15.0, 15.0, 15.0),
+      decoration: BoxDecoration(
+        color: Color(0xffFEFEFE),
+        borderRadius: BorderRadius.circular(10.0),
+        ),
+
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            GradientButton(
+              color1: Color(0xffE716A3),
+              color2: Color(0xff6C37CB),
+              size: 10.0,
+              icon: Icons.sports_basketball,
+            ),
+
+            Expanded(child: Text('Here is your ToDo.')),
+
+            SizedBox(width: 20.0,),
+
+            Column(
+              children: [
+                Text('18 Jun', style: TextStyle(fontWeight: FontWeight.bold),),
+                SizedBox(height: 5.0,),
+                Text('10:26'),
+              ],
+            ),
+          ],
+        ),
+    );
+  }
+}
