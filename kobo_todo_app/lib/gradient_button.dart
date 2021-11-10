9c08424d91d3b700234b2c65535893b6c4627ff4
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-10 02:10:20 +0100

    Created a class for the gradient buttons

---
+++ kobo_todo_app/lib/gradient_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class GradientButton extends StatelessWidget {
+  final Color color1;
+  final Color color2;
+  final IconData icon;
+  final Function()? action;
+  final double size;
+
+  const GradientButton({Key? key,
+    required this.color1,
+    required this.color2,
+    required this.icon,
+    this.action,
+    this.size = 15.0, }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: ShapeDecoration(
+        shape: CircleBorder(),
+        gradient: LinearGradient(
+          colors: [color1, color2],
+        ),
+      ),
+      child: MaterialButton(
+        padding: EdgeInsets.all(size),
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        child: Icon(icon, color: Colors.white,),
+        onPressed: action,
+      ),
+    );
+  }
+}
+
+
+
